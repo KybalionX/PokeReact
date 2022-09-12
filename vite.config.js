3818c6822a18415ac79989f27e2272aecae7cfd3
Nicolas <nicolaseduardoflorez1@gmail.com>
2022-09-11 19:00:57 -0500

    Changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		port: process.env.PORT || 5000,
-	},
+	server: {port: process.env.PORT || 5000},
 });
